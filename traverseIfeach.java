79037b8988420e82e22ae2af49b7abc6bf82ba77
MsShivi214 <114904628+MsShivi214@users.noreply.github.com>
2024-07-20 23:35:56 +0530

    Add files via upload

---
+++ traverseIfeach.java
@@ -0,0 +1,16 @@
+import java.util.*;  
+class Traverse
+{  
+  public static void main(String args[]){  
+   
+   ArrayList<String> list=new ArrayList<String>();  
+   list.add("shivangi");  
+   list.add("punya");  
+   list.add("bts");  
+   
+   for(String s:list){  
+     System.out.println(s);  
+   }  
+  
+ }   
+}  
